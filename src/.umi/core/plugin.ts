03154555d73167f9fe443d79975550bbe7f17e47
zjc <1742292412@qq.com>
2021-09-22 20:54:07 +0800

    feat: fist commit

--- src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/zhangjianchao/Desktop/react/myapp/node_modules/umi/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu','getInitialState','initialStateConfig','layout','layoutActionRef','request',],
-});
-
-export { plugin };
--- src/.umi/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from '/Users/zhangjianchao/Desktop/react/myapp/node_modules/react-helmet';
